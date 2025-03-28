6083dc8ce5513a13bf2aff157c739d52a2a297b9
Nathaniel-Haines <nathaniel.b.haines@gmail.com>
2025-03-28 11:16:12 -0400

    fix: test updates

--- test/integration/test_fit_predict.py
+++ test/integration/test_fit_predict.py
@@ -9,7 +9,7 @@ def test_fit_predict():
     client = AnalyticsClient()
     name = "__test_chain_ladder"
     chain_ladder = client.development_model.create(
-        triangle_name="meyers_clipped",
+        triangle="meyers_clipped",
         model_name="__test_chain_ladder",
         model_type="ChainLadder",
     )
@@ -18,15 +18,10 @@ def test_fit_predict():
     assert isinstance(model_from_client, DevelopmentModel)
     assert model_from_client.get_response.status_code == 200
     assert model_from_client.get_response.json()["name"] == name
-    chain_ladder.predict(triangle_name="meyers_clipped")
-    predictions = client.triangle.get(
-        triangle_id=chain_ladder.predict_response.json()["predictions"]
-    )
-    chain_ladder2 = client.development_model.predict(
+
+    predictions = chain_ladder.predict(triangle="meyers_clipped")
+    predictions2 = client.development_model.predict(
         triangle_name="meyers_clipped", model_name=name
-    )
-    predictions2 = client.triangle.get(
-        triangle_id=chain_ladder2.predict_response.json()["predictions"]
     )
     assert predictions.to_bermuda().extract("paid_loss").shape == (36, 10e3)
     assert predictions.to_bermuda() == predictions2.to_bermuda()
